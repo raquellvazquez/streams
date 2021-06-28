aa9bc9222c384aee4a1217775f9dd4033b69eab2
Raquel Vazquez Sanchez <raquelskrats@gmail.com>
2021-06-28 17:01:52 -0500

    feat: add redux form

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore } from "redux";
+import { createStore, compose, applyMiddleware } from "redux";
 import { Provider } from "react-redux";
 import reducers from './reducers/index';
 
 import App from './components/App';
-
-const store = createStore(reducers)
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(reducers, composeEnhancers(applyMiddleware()));
 
 ReactDOM.render(
     <Provider  store={store}>
