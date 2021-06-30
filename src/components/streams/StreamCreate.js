a42a9d1eb8ad0d6ac2d588a209abf331cfdec483
Raquel Vazquez Sanchez <raquelskrats@gmail.com>
2021-06-29 22:05:59 -0500

    feat: action creators request

--- src/components/streams/StreamCreate.js
+++ src/components/streams/StreamCreate.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { Field, reduxForm} from 'redux-form';
+import { connect } from 'react-redux';
+import { createStream } from '../../actions';
 class StreamCreate extends React.Component {
 
     renderError({error, touched}) {
@@ -23,9 +25,8 @@ class StreamCreate extends React.Component {
         )
     }
 
-    onSubmit(formValues) {
-        console.log('%cStreamCreate.js line:15 formValues', 'color: #007acc;', formValues);
-
+    onSubmit  = (formValues)  =>{
+        this.props.createStream(formValues);
     }
 
     render() { 
@@ -51,7 +52,10 @@ const validate = (formValues) => {
     return errors;
 }
 
-export default reduxForm({
+
+const formWrapped = reduxForm({
     form: 'streamCreate',
     validate: validate
 })(StreamCreate);
+
+export default connect(null, { createStream })(formWrapped)
--- src/index.js
+++ src/index.js
@@ -3,10 +3,11 @@ import ReactDOM from 'react-dom';
 import { createStore, compose, applyMiddleware } from "redux";
 import { Provider } from "react-redux";
 import reducers from './reducers/index';
+import reduxThunk  from 'redux-thunk';
 
 import App from './components/App';
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-const store = createStore(reducers, composeEnhancers(applyMiddleware()));
+const store = createStore(reducers, composeEnhancers(applyMiddleware(reduxThunk)));
 
 ReactDOM.render(
     <Provider  store={store}>
