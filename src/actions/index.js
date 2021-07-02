3b673b427421a00fecef6e199eecd9c3b738fc0b
Raquel Vazquez Sanchez <raquelskrats@gmail.com>
2021-07-01 20:44:39 -0500

    fix: add edit funtionality

--- src/actions/index.js
+++ src/actions/index.js
@@ -8,7 +8,7 @@ import {
     EDIT_STREAM
 } from './types';
 import streams from '../APIS/streams';
-import { formValues } from 'redux-form';
+import history from '../history';
 
 export const signIn = (userId) => {
     return {
@@ -31,7 +31,8 @@ export const createStream = formValues => async (dispatch, getState) => {
     dispatch({
         type: CREATE_STREAM,
         payload: response.data
-    })
+    });
+    history.push('/')
 };
 
 export const fetchStreams = () => async dispatch => {
@@ -53,12 +54,13 @@ export const fetchStream = (id) => async dispatch => {
 }
 
 export const editStream = (id, formValues) => async dispatch => {
-    const response = await streams.put(`/streams/${id}`, formValues);
+    const response = await streams.patch(`/streams/${id}`, formValues);
 
     dispatch({
         type: EDIT_STREAM,
         payload: response.data
     })
+    history.push('/');
 }
 
 export const deleteStream = (id) => async dispatch => {
